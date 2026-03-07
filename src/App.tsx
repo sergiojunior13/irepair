431b14e9208abd15f73aba5509b8a1ec9424a8bc
Sergio Junior <sergiaraujo15@gmail.com>
2026-03-07 17:42:21 -0300

    feat: adiciona a funcionalidade de mudar o status da ordem de serviço

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,20 @@ export function App() {
     const [servicesOrders, setServicesOrders] = useState<ServiceOrder[]>([]);
 
     function addServiceOrder(serviceOrder: ServiceOrder) {
-        setServicesOrders([...servicesOrders, serviceOrder]);
+        setServicesOrders((prevServices) => {
+            return [...prevServices, serviceOrder];
+        });
+    }
+
+    // Muda o status do serviço baseado em seu índice
+    function setStatus(status: ServiceOrder["status"], index: number) {
+        setServicesOrders((prevServices) =>
+            prevServices.map((service, i) => {
+                if (i === index) return { ...service, status: status };
+
+                return service;
+            }),
+        );
     }
 
     return (
@@ -24,25 +37,33 @@ export function App() {
                     <section className="flex-1">
                         <h2 className="font-bold text-center text-lg min-w-80">Abertas</h2>
                         <ul className="flex flex-col gap-2 mt-2">
-                            {servicesOrders
-                                .filter((so) => so.status === "open")
-                                .map((serviceOrder) => (
-                                    <li>
-                                        <ServiceCard {...serviceOrder} />
-                                    </li>
-                                ))}
+                            {servicesOrders.map(
+                                (serviceOrder, i) =>
+                                    serviceOrder.status === "open" && (
+                                        <li>
+                                            <ServiceCard
+                                                setStatus={(status) => setStatus(status, i)}
+                                                {...serviceOrder}
+                                            />
+                                        </li>
+                                    ),
+                            )}
                         </ul>
                     </section>
                     <section className="flex-1">
                         <h2 className="font-bold text-center text-lg min-w-80">Finalizadas</h2>
                         <ul className="flex flex-col gap-2 mt-2">
-                            {servicesOrders
-                                .filter((so) => so.status === "done")
-                                .map((serviceOrder) => (
-                                    <li>
-                                        <ServiceCard {...serviceOrder} status="done" />
-                                    </li>
-                                ))}
+                            {servicesOrders.map(
+                                (serviceOrder, i) =>
+                                    serviceOrder.status === "done" && (
+                                        <li>
+                                            <ServiceCard
+                                                setStatus={(status) => setStatus(status, i)}
+                                                {...serviceOrder}
+                                            />
+                                        </li>
+                                    ),
+                            )}
                         </ul>
                     </section>
                 </div>
--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,15 +1,31 @@
 import type { ServiceOrder } from "../types/ServiceOrder";
 
-export function ServiceCard({ clientName, defect, deviceModel, status }: ServiceOrder) {
+type ServiceCardProps = ServiceOrder & { setStatus: (status: ServiceOrder["status"]) => void };
+
+export function ServiceCard({ clientName, defect, deviceModel, status, setStatus }: ServiceCardProps) {
+    function handleChangeStatusBtnClick() {
+        if (status === "done") setStatus("open");
+        else setStatus("done");
+    }
+
     return (
         <div
             className={`flex flex-col gap-1 max-w-3xl mx-auto ${status === "open" ? "border-green-600 bg-green-200" : "border-zinc-600 bg-zinc-200"} border-2 rounded-xl p-3 shadow-blue-100 shadow-lg`}
         >
-            <span
-                className={`text-white w-min ${status === "open" ? "bg-green-600" : "bg-zinc-600"} p-1 px-2 rounded-full font-semibold`}
-            >
-                {status === "open" ? "Aberto" : "Finalizado"}
-            </span>
+            <div className="flex justify-between">
+                <span
+                    className={`text-white w-min ${status === "open" ? "bg-green-600" : "bg-zinc-600"} p-1 px-2 rounded-full font-semibold`}
+                >
+                    {status === "open" ? "Aberto" : "Finalizado"}
+                </span>
+
+                <button
+                    onClick={handleChangeStatusBtnClick}
+                    className="text-white bg-red-600 cursor-pointer hover:bg-red-800 transition-colors px-2 p-0.5 rounded-lg font-semibold inline"
+                >
+                    Mudar status
+                </button>
+            </div>
 
             <h3 className="font-extrabold first-letter:uppercase text-lg">{defect}</h3>
 
