96834a99a3cd743dde3e5e7cffb83b1d8da1c97a
Sergio Junior <sergiaraujo15@gmail.com>
2026-03-13 11:29:45 -0300

    fix(ServiceCard):  modifica o ServiceCard para usar a API

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,22 +1,69 @@
-import type { ServiceOrder } from "../types/serviceOrder";
+import { useEffect, useState } from "react";
+import type { NewServiceOrder, ServiceOrder } from "../types/serviceOrder";
+import type { Client } from "../types";
+import { getClient } from "../services/clientService";
+import { updateServiceOrder } from "../services/serviceOrderService";
 
 type ServiceCardProps = ServiceOrder & { setStatus: (status: ServiceOrder["status"]) => void };
 
-export function ServiceCard({ clientName, defect, deviceModel, status, createdAt, setStatus }: ServiceCardProps) {
+export function ServiceCard({ setStatus, ...serviceOrder }: ServiceCardProps) {
     function handleChangeStatusBtnClick() {
-        if (status === "done") setStatus("open");
-        else setStatus("done");
+        const updatedServiceOrder: NewServiceOrder = {
+            clientId: serviceOrder.client_id,
+            device: serviceOrder.device,
+            issue: serviceOrder.issue,
+            status: serviceOrder.status,
+        };
+
+        switch (serviceOrder.status) {
+            case "open":
+                updatedServiceOrder.status = "in_progress";
+                updateServiceOrder(serviceOrder.id, updatedServiceOrder);
+                setStatus("in_progress");
+                break;
+            case "in_progress":
+                updatedServiceOrder.status = "done";
+                updateServiceOrder(serviceOrder.id, updatedServiceOrder);
+                setStatus("done");
+                break;
+            case "done":
+                updatedServiceOrder.status = "open";
+                updateServiceOrder(serviceOrder.id, updatedServiceOrder);
+                setStatus("open");
+                break;
+        }
+    }
+
+    const [client, setClient] = useState<Client>();
+
+    useEffect(() => {
+        getClient(serviceOrder.client_id).then((c) => {
+            if (c) setClient(c);
+        });
+    });
+
+    let statusName;
+    switch (serviceOrder.status) {
+        case "open":
+            statusName = "Aberta";
+            break;
+        case "in_progress":
+            statusName = "Em andamento";
+            break;
+        case "done":
+            statusName = "Fechada";
+            break;
     }
 
     return (
         <div
-            className={`flex flex-col gap-1 max-w-3xl mx-auto ${status === "open" ? "border-green-600 bg-green-200" : "border-zinc-600 bg-zinc-200"} border-2 rounded-xl p-3 shadow-blue-100 shadow-lg`}
+            className={`flex flex-col gap-1 max-w-3xl mx-auto ${serviceOrder.status === "open" ? "border-green-600 bg-green-200" : "border-zinc-600 bg-zinc-200"} border-2 rounded-xl p-3 shadow-blue-100 shadow-lg`}
         >
             <div className="flex justify-between">
                 <span
-                    className={`text-white w-min ${status === "open" ? "bg-green-600" : "bg-zinc-600"} p-1 px-2 rounded-full font-semibold`}
+                    className={`text-white w-min ${serviceOrder.status === "open" ? "bg-green-600" : "bg-zinc-600"} p-1 px-2 rounded-full font-semibold text-xs whitespace-nowrap`}
                 >
-                    {status === "open" ? "Aberto" : "Finalizado"}
+                    {statusName}
                 </span>
 
                 <button
@@ -27,19 +74,19 @@ export function ServiceCard({ clientName, defect, deviceModel, status, createdAt
                 </button>
             </div>
 
-            <h3 className="font-extrabold first-letter:uppercase text-lg">{defect}</h3>
+            <h3 className="font-extrabold first-letter:uppercase text-lg">{serviceOrder.issue}</h3>
 
             <div className="flex gap-2">
                 <p>
-                    <span className="font-bold">Modelo:</span> {deviceModel}
+                    <span className="font-bold">Modelo:</span> {serviceOrder.device}
                 </p>
                 |
                 <p>
-                    <span className="font-bold">Cliente:</span> {clientName}
+                    <span className="font-bold">Cliente:</span> {client?.name}
                 </p>
             </div>
 
-            <p className="font-medium text-black/80 text-end text-sm">{createdAt.toLocaleString()}</p>
+            <p className="font-medium text-black/80 text-end text-sm">{serviceOrder.created_at.toLocaleString()}</p>
         </div>
     );
 }
