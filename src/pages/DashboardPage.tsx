d42d9cbe4b1787b33914cd36823735410cc0021f
Sergio Junior <sergiaraujo15@gmail.com>
2026-03-13 11:52:43 -0300

    feat(Dashboard): faz com que as OS agora venham da API

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,56 +1,72 @@
-import { useState } from "react";
-import { NewServiceForm } from "../components/NewServiceForm";
+import { useEffect, useState } from "react";
 import type { ServiceOrder } from "../types/serviceOrder";
 import { ServiceCard } from "../components/ServiceCard";
+import { getServiceOrders } from "../services/serviceOrderService";
 
 export function DashboardPage() {
     const [servicesOrders, setServicesOrders] = useState<ServiceOrder[]>([]);
 
-    function addServiceOrder(serviceOrder: ServiceOrder) {
-        setServicesOrders((prevServices) => {
-            return [...prevServices, serviceOrder];
-        });
-    }
+    useEffect(() => {
+        async function load() {
+            const so = await getServiceOrders();
+            if (!so) {
+                alert("Não foi possível obter as ordens de serviço.");
+                return;
+            }
 
-    // Muda o status do serviço baseado em seu índice
+            setServicesOrders(so);
+        }
+
+        load();
+    }, []);
+
     function setStatus(status: ServiceOrder["status"], index: number) {
-        setServicesOrders((prevServices) =>
-            prevServices.map((service, i) => {
-                if (i === index) return { ...service, status: status };
-
-                return service;
+        setServicesOrders((prev) =>
+            // Se for o indíce da OS a mudar, altera o status,
+            // se não, somente retorna a original
+            prev.map((so, i) => {
+                if (i === index) return { ...so, status };
+                return so;
             }),
         );
     }
 
-    return (
-        <div className="max-w-5xl mx-auto px-6">
-            <section>
-                <NewServiceForm addServiceOrder={addServiceOrder} />
-            </section>
+    function removeSO(index: number) {
+        setServicesOrders((prev) => prev.filter((so, i) => i !== index));
+    }
 
-            <div className="mt-7 flex flex-wrap gap-3 justify-between max-w-5xl mx-auto">
+    return (
+        <div className="mx-auto px-6 flex-1 w-full">
+            <div className="mt-7 flex flex-wrap gap-3 justify-between max-w-7xl mx-auto">
                 <section className="flex-1">
-                    <h2 className="font-bold text-center text-lg min-w-80">Abertas</h2>
+                    <h2 className="font-bold text-center text-lg">Abertas</h2>
                     <ul className="flex flex-col gap-2 mt-2">
                         {servicesOrders.map(
                             (serviceOrder, i) =>
                                 serviceOrder.status === "open" && (
                                     <li>
-                                        <ServiceCard setStatus={(status) => setStatus(status, i)} {...serviceOrder} />
+                                        <ServiceCard
+                                            onDelete={() => removeSO(i)}
+                                            setStatus={(status) => setStatus(status, i)}
+                                            {...serviceOrder}
+                                        />
                                     </li>
                                 ),
                         )}
                     </ul>
                 </section>
                 <section className="flex-1">
-                    <h2 className="font-bold text-center text-lg min-w-80">Finalizadas</h2>
+                    <h2 className="font-bold text-center text-lg">Finalizadas</h2>
                     <ul className="flex flex-col gap-2 mt-2">
                         {servicesOrders.map(
                             (serviceOrder, i) =>
                                 serviceOrder.status === "done" && (
                                     <li>
-                                        <ServiceCard setStatus={(status) => setStatus(status, i)} {...serviceOrder} />
+                                        <ServiceCard
+                                            onDelete={() => removeSO(i)}
+                                            setStatus={(status) => setStatus(status, i)}
+                                            {...serviceOrder}
+                                        />
                                     </li>
                                 ),
                         )}
