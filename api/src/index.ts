4c37e4b6f4a89ccfcabd590eb2d4902685e3ca69
Sergio Junior <sergiaraujo15@gmail.com>
2026-04-02 21:29:08 -0300

    feat: adiciona verificações se as variáveis de ambiente essenciais existem

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,12 @@
 import "dotenv/config";
+
+if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET não definido nas variáveis de ambiente");
+}
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL não definido nas variáveis de ambiente");
+}
+
 import { app } from "./config/expressConfig";
 
 app.listen(process.env.PORT, () => {
