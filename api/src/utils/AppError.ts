0cc1c5d3150c32b097146557745ac9b8ec09f2ed
Sergio Junior <sergiaraujo15@gmail.com>
2026-04-03 12:15:43 -0300

    feat: adiciona o AuthService

---
+++ api/src/utils/AppError.ts
@@ -0,0 +1,10 @@
+export class AppError extends Error {
+    statusCode: number;
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.message = message;
+        this.statusCode = statusCode;
+        this.name = "AppError";
+    }
+}
